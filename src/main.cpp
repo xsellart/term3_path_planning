3caa3a493ce0f03c6fc2a88f22559c483d0cbfa5
Xavi <xsellart1985@gmail.com>
2017-10-03 21:38:57 +0200

    I've finished the prediction of vehicles from data fusion module. I've just also implemented a find_lane method.

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 #include "Eigen-3.3/Eigen/QR"
 #include "json.hpp"
 #include "spline.h"
+#include "vehicle.h"
 
 using namespace std;
 
@@ -253,177 +254,207 @@ int main() {
   		
 
 
-          	// TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
+        // TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
 		
-		int prev_size = previous_path_x.size(); // size of the waypoints not achieved during the las iteration
+		// Firstly, we will generate a vector of predictions from the sensor fusion module.
+        
+        vector<Vehicle> target_vehicles;
+        map<int, vector<vector<int>>> predictions;
+
+        //cout << "Host Vehicle Distance " << car_d << endl;
+        //cout << "Number of target vehicles " << sensor_fusion.size() << endl;
+
+        for(int i=0; i < sensor_fusion.size(); i++){
+   			
+   			double tv_vx = sensor_fusion[i][3];
+			double tv_vy = sensor_fusion[i][4];
+			double tv_vel = sqrt(tv_vx*tv_vx+tv_vy*tv_vy);
+			double tv_s = sensor_fusion[i][5];
+
+			Vehicle target_vehicle = Vehicle(0,tv_s, tv_vel,0);
+			target_vehicle.lane = target_vehicle.find_lane(sensor_fusion[i][6]);
+			cout << "Lane  " << target_vehicle.lane << endl;
+
+   			target_vehicles.push_back(target_vehicle);
+
+			int tv_id = sensor_fusion[i][0];
+			int prediction_duration = 10;
+			vector<vector<int>> preds = target_vehicle.generate_predictions(prediction_duration);
+			predictions[tv_id] = preds;
+		}
+
+		cout << "Number of predictions " << predictions.size() << endl;
+
+
+		// int prev_size = previous_path_x.size(); // size of the waypoints not achieved during the last iteration
 		
-		std::cout << "prev_size " << prev_size << std::endl;
-		// These are the spaced (x,y) waypoints
-		vector<double> ptsx;
-		vector<double> ptsy;
+		// std::cout << "prev_size " << prev_size << std::endl;
+		// // These are the spaced (x,y) waypoints
+		// vector<double> ptsx;
+		// vector<double> ptsy;
 
-		// Reference State
-		double ref_x = car_x;
-		double ref_y = car_y;
-		double ref_yaw = deg2rad(car_yaw);
+		// // Reference State
+		// double ref_x = car_x;
+		// double ref_y = car_y;
+		// double ref_yaw = deg2rad(car_yaw);
 
-		// We have to check if there are vehicles in our path and decide what to do
-		// We have to use the Sensor fusion data
+		// // We have to check if there are vehicles in our path and decide what to do
+		// // We have to use the Sensor fusion data
 
-		if (prev_size > 0)
-	   	{
-			car_s = end_path_s;
-		}
+		// if (prev_size > 0)
+	 //   	{
+		// 	car_s = end_path_s;
+		// }
 
-		bool too_close = false;
+		// bool too_close = false;
 
-		for (int i = 0; i < sensor_fusion.size();i++)
-		{
-		// Check if the cars are in our lane
-		float d = sensor_fusion[i][6];  		
+		// for (int i = 0; i < sensor_fusion.size();i++)
+		// {
+		// // Check if the cars are in our lane
+		// float d = sensor_fusion[i][6];  		
 		
-		if ((d < (2+4*lane+2)) && (d > (2+4*lane-2)))
-		{
-		double vx = sensor_fusion[i][3];
-		double vy = sensor_fusion[i][4];
-		double check_speed = sqrt(vx*vx+vy*vy);
-		double check_car_s = sensor_fusion[i][5];
+		// if ((d < (2+4*lane+2)) && (d > (2+4*lane-2)))
+		// {
+		// double vx = sensor_fusion[i][3];
+		// double vy = sensor_fusion[i][4];
+		// double check_speed = sqrt(vx*vx+vy*vy);
+		// double check_car_s = sensor_fusion[i][5];
 
-		check_car_s+=((double)prev_size*.02*check_speed);
+		// check_car_s+=((double)prev_size*.02*check_speed);
 
-		if ((check_car_s > car_s) && (check_car_s - car_s < 30))
-		{
-		std::cout << "check_car_s " << ref_vel << std::endl;
-		//ref_vel = 29;
-		too_close = true;
+		// if ((check_car_s > car_s) && (check_car_s - car_s < 30))
+		// {
+		// std::cout << "check_car_s " << ref_vel << std::endl;
+		// //ref_vel = 29;
+		// too_close = true;
 
-		if (lane > 0){
-		lane = 0;
-		}
-		}
+		// if (lane > 0){
+		// lane = 0;
+		// }
+		// }
 
 
-		if (too_close)
-		{
-		std::cout << "Inside too_close " << ref_vel << std::endl;
-		ref_vel-=.224;
-		}else if (ref_vel < 49)
-		{
-		std::cout << "Inside elseif " << ref_vel << std::endl;
-		ref_vel+=.224;
-		}
+		// if (too_close)
+		// {
+		// std::cout << "Inside too_close " << ref_vel << std::endl;
+		// ref_vel-=.224;
+		// }else if (ref_vel < 49)
+		// {
+		// std::cout << "Inside elseif " << ref_vel << std::endl;
+		// ref_vel+=.224;
+		// }
 
-		}
+		// }
 		
 		
 
-		}
+		// }
 
 
-		// We have to check previous path to see if its empty or we can use past waypoints
-		// for determining our orientation
+		// // We have to check previous path to see if its empty or we can use past waypoints
+		// // for determining our orientation
 		
-		if (prev_size < 2){
+		// if (prev_size < 2){
 
-			double prev_car_x = car_x - cos(car_yaw);
-			double prev_car_y = car_y - sin(car_yaw);
+		// 	double prev_car_x = car_x - cos(car_yaw);
+		// 	double prev_car_y = car_y - sin(car_yaw);
 
-			ptsx.push_back(prev_car_x);
-			ptsx.push_back(car_x);
-			ptsy.push_back(prev_car_y);
-			ptsy.push_back(car_y);
-		}
+		// 	ptsx.push_back(prev_car_x);
+		// 	ptsx.push_back(car_x);
+		// 	ptsy.push_back(prev_car_y);
+		// 	ptsy.push_back(car_y);
+		// }
 
-		else {
+		// else {
 
-			ref_x = previous_path_x [prev_size - 1];
-			ref_y = previous_path_y [prev_size -1];
+		// 	ref_x = previous_path_x [prev_size - 1];
+		// 	ref_y = previous_path_y [prev_size -1];
 
-			double ref_x_prev = previous_path_x [prev_size - 2];
-			double ref_y_prev = previous_path_y [prev_size - 2];
+		// 	double ref_x_prev = previous_path_x [prev_size - 2];
+		// 	double ref_y_prev = previous_path_y [prev_size - 2];
 
-			ref_yaw = atan2 (ref_y - ref_y_prev,ref_x -ref_x_prev);
+		// 	ref_yaw = atan2 (ref_y - ref_y_prev,ref_x -ref_x_prev);
 
-			ptsx.push_back(ref_x_prev);
-			ptsx.push_back(ref_x);
-			ptsy.push_back(ref_y_prev);
-			ptsy.push_back(ref_y);
+		// 	ptsx.push_back(ref_x_prev);
+		// 	ptsx.push_back(ref_x);
+		// 	ptsy.push_back(ref_y_prev);
+		// 	ptsy.push_back(ref_y);
 
-		}
+		// }
 
-		//std::cout << "ref_x " << ref_x << std::endl;
-		//std::cout << "ref_y " << ref_y << std::endl;
+		// //std::cout << "ref_x " << ref_x << std::endl;
+		// //std::cout << "ref_y " << ref_y << std::endl;
 	
-		// We have to create 3 points with 30m of distance between them
-		vector<double> next_wp0 = getXY(car_s+30,(2+4*lane),map_waypoints_s,map_waypoints_x,map_waypoints_y);
-		vector<double> next_wp1 = getXY(car_s+60,(2+4*lane),map_waypoints_s,map_waypoints_x,map_waypoints_y);
-		vector<double> next_wp2 = getXY(car_s+90,(2+4*lane),map_waypoints_s,map_waypoints_x,map_waypoints_y);
+		// // We have to create 3 points with 30m of distance between them
+		// vector<double> next_wp0 = getXY(car_s+30,(2+4*lane),map_waypoints_s,map_waypoints_x,map_waypoints_y);
+		// vector<double> next_wp1 = getXY(car_s+60,(2+4*lane),map_waypoints_s,map_waypoints_x,map_waypoints_y);
+		// vector<double> next_wp2 = getXY(car_s+90,(2+4*lane),map_waypoints_s,map_waypoints_x,map_waypoints_y);
 
-		ptsx.push_back(next_wp0[0]);
-		ptsx.push_back(next_wp1[0]);
-		ptsx.push_back(next_wp2[0]);
+		// ptsx.push_back(next_wp0[0]);
+		// ptsx.push_back(next_wp1[0]);
+		// ptsx.push_back(next_wp2[0]);
 
-		ptsy.push_back(next_wp0[1]);
-		ptsy.push_back(next_wp1[1]);
-		ptsy.push_back(next_wp2[1]);
+		// ptsy.push_back(next_wp0[1]);
+		// ptsy.push_back(next_wp1[1]);
+		// ptsy.push_back(next_wp2[1]);
 
-		// Shift from global coordinates to local coordiantes of the vehicle
+		// // Shift from global coordinates to local coordiantes of the vehicle
 
-		for (int i = 0; i < ptsx.size(); i++)
-		{
-			double shift_x = ptsx[i] - ref_x;
-			double shift_y = ptsy[i] - ref_y;
+		// for (int i = 0; i < ptsx.size(); i++)
+		// {
+		// 	double shift_x = ptsx[i] - ref_x;
+		// 	double shift_y = ptsy[i] - ref_y;
 
-			ptsx[i] = (shift_x*cos(0-ref_yaw) - (shift_y*sin(0-ref_yaw)));
-			ptsy[i] = (shift_x*sin(0-ref_yaw) + (shift_y*cos(0-ref_yaw)));
-		}
+		// 	ptsx[i] = (shift_x*cos(0-ref_yaw) - (shift_y*sin(0-ref_yaw)));
+		// 	ptsy[i] = (shift_x*sin(0-ref_yaw) + (shift_y*cos(0-ref_yaw)));
+		// }
 		
 
-		// Create the spline to join waypoints between reference waypoints		
-		tk::spline s;
+		// // Create the spline to join waypoints between reference waypoints		
+		// tk::spline s;
 
-		//std::cout << "ptsx " << ptsx.size() << std::endl;
-		s.set_points(ptsx,ptsy);
-		//std::cout << "After set points " << std::endl;
-		// We fill the previous path
+		// //std::cout << "ptsx " << ptsx.size() << std::endl;
+		// s.set_points(ptsx,ptsy);
+		// //std::cout << "After set points " << std::endl;
+		// // We fill the previous path
 
-		for (int i = 0; i < previous_path_x.size();i++)
-		{
-			next_x_vals.push_back(previous_path_x[i]);
-			next_y_vals.push_back(previous_path_y[i]);
-		}
+		// for (int i = 0; i < previous_path_x.size();i++)
+		// {
+		// 	next_x_vals.push_back(previous_path_x[i]);
+		// 	next_y_vals.push_back(previous_path_y[i]);
+		// }
 
-		// In this part, we have to find the trajectory waypoints with split
+		// // In this part, we have to find the trajectory waypoints with split
 
-		double target_x = 30.0; // 30 meters 
-		double target_y = s(target_x); // To find y value with spline
-		double target_distance = sqrt((target_x*target_x)+(target_y*target_y));
+		// double target_x = 30.0; // 30 meters 
+		// double target_y = s(target_x); // To find y value with spline
+		// double target_distance = sqrt((target_x*target_x)+(target_y*target_y));
 
-		double x_add_on = 0;
+		// double x_add_on = 0;
 
-		for (int i = 1; i <= 50-previous_path_x.size();i++)
-		{
-			double N = (target_distance/(0.02*ref_vel/2.24));
+		// for (int i = 1; i <= 50-previous_path_x.size();i++)
+		// {
+		// 	double N = (target_distance/(0.02*ref_vel/2.24));
 
-			double x_point = x_add_on + (target_x)/N;
-			double y_point = s(x_point);
+		// 	double x_point = x_add_on + (target_x)/N;
+		// 	double y_point = s(x_point);
 
-			x_add_on = x_point;
+		// 	x_add_on = x_point;
 
-			double x_ref = x_point;
-			double y_ref = y_point;
+		// 	double x_ref = x_point;
+		// 	double y_ref = y_point;
 
-		// Rotation to global coordinates
+		// // Rotation to global coordinates
 
-			x_point = (x_ref*cos(ref_yaw)-y_ref*sin(ref_yaw));
-			y_point = (x_ref*sin(ref_yaw)+y_ref*cos(ref_yaw)); 
+		// 	x_point = (x_ref*cos(ref_yaw)-y_ref*sin(ref_yaw));
+		// 	y_point = (x_ref*sin(ref_yaw)+y_ref*cos(ref_yaw)); 
 
-			x_point += ref_x;
-			y_point += ref_y;
+		// 	x_point += ref_x;
+		// 	y_point += ref_y;
 
-			next_x_vals.push_back(x_point);
-			next_y_vals.push_back(y_point);
-		}
+		// 	next_x_vals.push_back(x_point);
+		// 	next_y_vals.push_back(y_point);
+		// }
 	
           	msgJson["next_x"] = next_x_vals;
           	msgJson["next_y"] = next_y_vals;
